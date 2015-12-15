d697a70e60eadbf2b5f06193e712e0211975f427
Andrea Ferlini <AndreaFerlini@MacBook-Pro-di-Ferla.local>
2015-12-15 17:27:32 +0100

    esame final

--- js/app.js
+++ js/app.js
@@ -3,22 +3,38 @@
  * fulfilled
  */
 
+var temp = "<li>OPT</li>";
+
+$(document).ready(function(){
+    Controller.init();
+});
+
 var SantaModel = {
   
   /* Initializes the model with a list of requests, 
    * and sets the first one as the current one 
    */
    init : function(list){
+       this.collection = requests;
+       this.points = 0;
+       this.current = 0;
    },
   
    /* It moves "current" to the next request */
    next : function (){
+       this.current = this.current ++;
+       if (this.current == this.collection.length){
+           Controller.showResult();
+       }else{
+           Controller.NextReq();
+       }
    },
   
    /* Returns the current request. 
     * If all requests have been processed (there is no current one), it returns null 
     */
    getCurrentRequest : function () {
+       return SantaModel.collection[this.current];
    },  
     
    /* Packs the given item if it fulfills the current request.       
@@ -26,6 +42,78 @@ var SantaModel = {
     * returns 0 if the given item does not fulfill the request
     */
    pack : function(item) {
-   }      
+       if (item == SantaModel.collection[this.current].answer){
+           return 1;
+       }else{
+           return 0;
+       }
+   },
+    
+   addPoints : function(ans){
+       var value = this.pack(ans);
+       this.points = this.points + value;
+   }
   
 };
+
+var Controller = {
+    init : function(){
+        SantaModel.init();
+        View.init();
+    },
+    GetRequest : function (){
+        var request;
+        request = SantaModel.getCurrentRequest();
+        return request;
+    },
+    GetCurrent : function (){
+        return SantaModel.current;
+    },
+    GetPoints : function (){
+        return SantaModel.points;
+    },
+    
+    SetPoints : function(ans){
+        SantaModel.addPoints(ans);
+    },
+    CallNext : function(){
+        SantaModel.next();
+    },
+    NextReq : function(){
+        View.init();
+    },
+    showResult : function(){
+        View.result();
+    }
+};
+
+var View = {
+    init : function(){
+        $(".question").empty();
+        $(".question-items").empty();
+        $(".result").empty();
+        var req;
+        var opt;
+        var ans;
+        
+        req = Controller.GetRequest();
+        $(".question").append(req.question);
+        for (var i = 0; i<req.options.length; i++){
+            opt = temp.replace("OPT", req.options[i]);
+            $(".question-items").append(opt);
+        }
+        $(".question-items li").click(function(){
+            ans = $(".question-items li").text();
+            Controller.SetPoints(ans);
+            Controller.CallNext();
+        })
+    },
+    result : function(){
+        $(".question").empty();
+        $(".question-items").empty();
+        $(".result").empty();
+        
+        $(".result").html("Total points: "+Controller.GetPoints());
+    }
+};
+
--- test/spec/app.spec.js
+++ test/spec/app.spec.js
@@ -3,8 +3,53 @@
 
 describe("Santa", function() {
 
-  it("should...", function() {  
-    
+  it("SantaModel.init should work correctly", function() {  
+    SantaModel.init();
+      
+    expect(SantaModel.collection).toEqual(requests);
+    expect(SantaModel.points).toBe(0);
+    expect(SantaModel.current).toBe(0);
   });    
  
+  it("SantaModel.next should work correctly", function() {  
+    SantaModel.next();
+    var value1 = SantaModel.current;
+    
+    SantaModel.init(); 
+    SantaModel.next();
+    var value2 = SantaModel.current;
+    
+    expect(value1).toBe(null); //no init, so next should return an error
+    expect(value2).toBe(1);
+  });
+  it("SantaModel.getCurrentRequest should work correctly", function() {  
+    SantaModel.init(); 
+    
+    expect(SantaModel.getCurrentRequest()).toEqual(requests[0]);
+    SantaModel.next(); //current = 1
+    expect(SantaModel.getCurrentRequest()).toEqual(requests[SantaModel.current]);
+    SantaModel.next(); //current = 2
+    SantaModel.next(); //current = 3
+    expect(SantaModel.getCurrentRequest()).toEqual(undefined);
+  });    
+ 
+  it("SantaModel.pack should work correctly", function() {  
+    SantaModel.init(); 
+    var ans = "yes";
+    var value1 = SantaModel.pack(ans);
+    ans = "hello";
+    var value2 = SantaModel.pack(ans);
+    ans = "no";
+    var value3 = SantaModel.pack(ans);
+    ans = 1;
+    var value4 = SantaModel.pack(ans);
+    ans = "No"; 
+    var value5 = SantaModel.pack(ans);
+      
+    expect(value1).toBe(0);
+    expect(value2).toBe(0); 
+    expect(value3).toBe(1); 
+    expect(value4).toBe(null); //error in input 
+    expect(value5).toBe(0); //answers in requirements are "no" or "yes" and not "No" or "Yes"
+  });
 });
